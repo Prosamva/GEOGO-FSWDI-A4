8ef11f457812d3983115079a315b8886f974d1a8
EXE-SV7 <samuelstar4@gmail.com>
2021-12-15 21:15:44 +0530

    Tasks Implemented, README added

---
+++ README.md
@@ -0,0 +1,18 @@
+# Assignment 4 - Creating an API Server for Blogs using Node.js &amp; MongoDB
+### Full Stack Web Development Internship
+#### GEOGO Techsolutions Pvt. Ltd.<br><br>
+
+The structure of the data is defined by the following schema:
+
+    {
+    	title:   String,
+    	author:  String,
+    	content: String,
+    	date: {type: Date, default: Date.now},
+    	hidden: {type: Boolean, default: false}
+    }
+
+#### API calls:
+- `/`<br>**GET**: Default message
+- `/posts`<br>**GET**: To get all posts or filtered results with query parameters(title, author).<br>**POST**: To post new blog.
+- `/posts/:id`<br>**GET**: Get single post by its ID.<br>**PUT**: Update single post by its ID.<br>**DELETE**: Delete single post by its ID.
--- index.js
+++ index.js
@@ -16,15 +16,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function(req, res) {
   // handle the request for root route
-  res.send({ ping: 'pong' })
+  res.send({ 'student': 'Samuel Vasamsetti', 'team': 'Kalam' })
 })
 
 // Operations: Create, Read, Update, Delete (CRUD)
 app.post('/posts', function(req, res) {
-  // Get values from request payload
-  const title = req.body.title
-  const author = req.body.author
-  const content = req.body.content
 
   // Assign values to Post model
   var post = new Post();
@@ -38,29 +34,72 @@ app.post('/posts', function(req, res) {
       // send error response
       res.status(500).send({ error: 'Unable to save Post '})
     } else {
-      // send success response
       res.status(200).send(savedPost)
     }
   })
 });
 
-// Get list of all posts
-app.get('/posts', function(req, res) {
-  Post.find({}, function(error, posts) {
-    if(error) {
-      // send error response
-      res.status(422).send({ error: 'Unable to fetch posts '})
-    } else {
-      // send success response
-      res.status(200).send(posts)
-    }
+// Removed for improved implementation below.
+// // Get list of all posts
+// app.get('/posts', function(req, res) {
+//   Post.find({}, function(error, posts) {
+//     if(error) {
+//       // send error response
+//       res.status(422).send({ error: 'Unable to fetch posts '})
+//     } else {
+//       // send success response
+//       res.status(200).send(posts)
+//     }
+//   })
+// })
+
+// TASKS
+// 1. Create API to get details of a single Post
+app.get('/posts/:id', function(req, res) {
+  Post.findById(req.params.id, function(error, post, ) {
+    if(error) 
+      res.status(422).send({error: 'Unable to fetch post!'})
+    else 
+      res.status(200).send(post)
   })
 })
 
-// Tasks for you
-// 1. Create API to get details of a single Post
+// Get all posts or provide query parameters to filter results.
+// query params: title, author
+app.get('/posts', function(req, res) {
+  const title = req.query.title
+  const author = req.query.author
+  var filter = {}
+  if (title!=null) 
+    filter['title'] = title
+  if (author!=null) 
+    filter['author'] = author
+  Post.find(filter, function(error, post) {
+    if(error) 
+      res.status(500).send({error: 'Unable to fetch post(s)!'})
+    else 
+      res.status(200).send(post)
+  })
+})
+
 // 2. Create API to update a Post
+app.put('/posts/:id', function(req, res) {
+  Post.findByIdAndUpdate(req.params.id, req.body, function (error, oldPost){
+    if(error) 
+      res.status(422).send({error: 'Update unsucessful!'})
+    else 
+      res.status(200).send({'beforeUpdate': oldPost})
+  })
+})
 // 3. Create API to delete a Post
+app.delete('/posts/:id', function(req, res) {
+  Post.findByIdAndDelete(req.params.id, function(error, post){
+    if(error)
+      res.status(422).send({error: 'Delete unsuccessful!'})
+    else
+      res.status(200).send({'deleted':post})
+  })
+})
 
 app.listen(3001, function() {
   console.log('Server is running at port 3001....')
